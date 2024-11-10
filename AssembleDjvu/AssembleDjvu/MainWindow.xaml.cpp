c60b7c285372a172a66dee55879ed819764ddf1c
ackasaber <ackasaber@gmail.com>
2024-11-10 21:42:26 +0300

    Open a file selection dialog via COM.

--- AssembleDjvu/AssembleDjvu/MainWindow.xaml.cpp
+++ AssembleDjvu/AssembleDjvu/MainWindow.xaml.cpp
@@ -3,6 +3,9 @@
 #if __has_include("MainWindow.g.cpp")
 #include "MainWindow.g.cpp"
 #endif
+#include <Shobjidl.h>
+#include <iterator>
+#include <microsoft.ui.xaml.window.h>
 
 namespace winrt::AssembleDjvu::implementation
 {
@@ -13,6 +16,21 @@ namespace winrt::AssembleDjvu::implementation
     void MainWindow::ExecuteAddCommand(XamlUICommand const&, ExecuteRequestedEventArgs const&)
     {
         OutputDebugString(L"TODO AddCommand\n");
+        auto dialog = winrt::create_instance<IFileOpenDialog>(guid_of<FileOpenDialog>());
+        winrt::check_hresult(dialog->SetTitle(L"Choose a JPEG image"));
+        COMDLG_FILTERSPEC fileTypes[] = {
+            { L"JPEG images", L"*.jpg;*.jpeg"},
+            { L"All files", L"*.*" }
+        };
+        winrt::check_hresult(dialog->SetFileTypes(std::size(fileTypes), fileTypes));
+        auto nativeWindow = this->try_as<IWindowNative>();
+        HWND parentHwnd{ 0 };
+        winrt::check_hresult(nativeWindow->get_WindowHandle(&parentHwnd));
+        HRESULT choiceHR = dialog->Show(parentHwnd);
+        if (SUCCEEDED(choiceHR))
+            OutputDebugString(L"Picked one");
+        else
+            OutputDebugString(L"Didn't pick?");
     }
 
     void MainWindow::ExecuteConvertCommand(XamlUICommand const&, ExecuteRequestedEventArgs const&)
